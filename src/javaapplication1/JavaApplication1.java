94192ed2334be7bcabb7525f7e5bbb06ec9e89f5
bregmesl <bregmesl@S206-16.3il.fr>
2015-03-25 15:46:45 +0100

    création de la fonction démarer

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -17,5 +17,8 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    public int demarer(){
+        return 0;
+    }
     
 }
